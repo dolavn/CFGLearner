fb03628a8bd9d0e8854863b247997b60b19934e6
Dolav Nitay <dolavn16@gmail.com>
2019-01-27 19:08:52 +0200

    Updated ParseTree tests

--- tests/basic_tests/TreeTest.cpp
+++ tests/basic_tests/TreeTest.cpp
@@ -165,12 +165,80 @@ TEST(tree_test,make_context){
     std::pair<ParseTree*,ParseTree*> contextPair = t.makeContext("0");
     ParseTree* context = contextPair.first;
     ParseTree* tree = contextPair.second;
+    ASSERT_EQ((*context)[""].getContextLoc(),"0");
+    ASSERT_EQ((*context)[""].getIsContext(),true);
     ASSERT_EQ((*context)[""].getData(),2);
     ASSERT_EQ((*context)["0"].getData(),-1);
+    ASSERT_EQ((*context)["0"].getContextLoc(),"");
+    ASSERT_EQ((*context)["0"].getIsContext(),true);
     ASSERT_EQ((*context)["1"].getData(),4);
+    ASSERT_EQ((*context)["1"].getContextLoc(),"");
+    ASSERT_EQ((*context)["1"].getIsContext(),false);
     ASSERT_EQ((*tree)[""].getData(),3);
+    ASSERT_EQ((*tree)[""].getContextLoc(),"");
+    ASSERT_EQ((*tree)[""].getIsContext(),false);
     ASSERT_EQ((*tree)["0"].getData(),2);
+    ASSERT_EQ((*tree)["0"].getContextLoc(),"");
+    ASSERT_EQ((*tree)["0"].getIsContext(),false);
     ASSERT_EQ((*tree)["1"].getData(),3);
+    ASSERT_EQ((*tree)["1"].getContextLoc(),"");
+    ASSERT_EQ((*tree)["1"].getIsContext(),false);
     delete(context);
     delete(tree);
+}
+
+TEST(tree_test,context_merge_test){
+    ParseTree t(2);
+    ParseTree t2(3);
+    t2.setLeftSubtree(ParseTree(2));
+    t2.setRightSubtree(ParseTree(3));
+    ParseTree t3(4);
+    t.setLeftSubtree(t2);
+    t.setRightSubtree(t3);
+    std::pair<ParseTree*,ParseTree*> contextPair = t.makeContext("0");
+    ParseTree* context = contextPair.first;
+    ParseTree* tree = contextPair.second;
+    ParseTree* merged = context->mergeContext(*tree);
+    ParseTree* merged2 = context->mergeContext(ParseTree(4,{ParseTree(2),ParseTree(1)}));
+    ASSERT_EQ(*merged==t,true);
+    ASSERT_EQ(merged->getIsContext(),false);
+    ASSERT_EQ(*merged2==t,false);
+    ASSERT_EQ(*merged2==ParseTree(2,{ParseTree(4,{ParseTree(2),ParseTree(3)}),ParseTree(4)}),false);
+    ASSERT_EQ(*merged2==ParseTree(2,{ParseTree(4,{ParseTree(2),ParseTree(1)}),ParseTree(4)}),true);
+    try{
+        merged->mergeContext(t);
+        ASSERT_EQ(1,0);
+    }catch(std::invalid_argument& e){
+        ASSERT_EQ(e.what(),std::string("Tree must be a context"));
+    }
+    delete(merged);
+    delete(merged2);
+    delete(context);
+    delete(tree);
+}
+
+TEST(tree_test,equivalence_test){
+    ParseTree t(2);
+    ParseTree t2(2);
+    ParseTree t3(3);
+    ASSERT_EQ(t==t2,true);
+    ASSERT_EQ(t==t3,false);
+    ASSERT_EQ(t!=t2,false);
+    ASSERT_EQ(t!=t3,true);
+    ParseTree t4(2,{t,t2});
+    ParseTree t5(2,{t,t3});
+    ASSERT_EQ(t4==t5,false);
+    ASSERT_EQ(t4!=t5,true);
+    ParseTree t6(2);
+    ParseTree t7(2);
+    t6.setLeftSubtree(t2);
+    t7.setLeftSubtree(t3);
+    ASSERT_EQ(t6==t7,false);
+    ASSERT_EQ(t6!=t7,true);
+    ParseTree t8(2);
+    ParseTree t9(2);
+    t8.setRightSubtree(t2);
+    t9.setRightSubtree(t2);
+    ASSERT_EQ(t8==t9,true);
+    ASSERT_EQ(t8!=t9,false);
 }
